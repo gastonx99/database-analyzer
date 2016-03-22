c9f82d6792d388c2b2fcb404dc3abcf8e53d20e9
gaston <dan.anders.lundberg@server.fake>
2016-03-22 21:04:20 +0100

    Updated environment for new PC

--- local-tools/project-bash.bat
+++ local-tools/project-bash.bat
@@ -1,7 +1,12 @@
 @echo off
 
 SET CURRENTDIR="%~dp0"
-set BASHHOME=C:\Users\gaston\AppData\Local\Atlassian\SourceTree\git_local
+set BASHHOME=C:\Progra~1\Git
+set MAVEN_HOME=D:\java\apache-maven-3.3.9
+set ANT_HOME=D:\java\apache-ant-1.9.6
+set JAVA_HOME=C:\Progra~1\Java\jdk1.8.0_73
+
+set PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%ANT_HOME%\bin;%PATH%
 
 cd %CURRENTDIR%\..
 start %BASHHOME%\git-bash.exe

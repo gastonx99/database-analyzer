7600b11b8deef7210dd11dd0c1b2c6861247f3f2
Anders Lundberg <dan.anders.lundberg@server.fake>
2018-06-25 19:07:25 +0200

    Updated to latest versions of java, maven and ant

--- local-tools/project-bash.bat
+++ local-tools/project-bash.bat
@@ -2,9 +2,9 @@
 
 SET CURRENTDIR="%~dp0"
 set BASHHOME=C:\Progra~1\Git
-set MAVEN_HOME=D:\java\apache-maven-3.3.9
-set ANT_HOME=D:\java\apache-ant-1.9.6
-set JAVA_HOME=C:\Progra~1\Java\jdk1.8.0_73
+set MAVEN_HOME=D:\java\apache-maven-3.5.4
+set ANT_HOME=D:\java\apache-ant-1.9.12
+set JAVA_HOME=C:\Progra~1\Java\jdk1.8.0_112
 
 set PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%ANT_HOME%\bin;%PATH%
 
